6719c2dd9ba7eb913f865709df6fcd46005597d3
steveljko <steveljko@proton.me>
2025-09-10 20:53:09 +0200

    add storage helper

---
+++ src/helpers/storage.ts
@@ -0,0 +1,23 @@
+import browser from "webextension-polyfill";
+
+type StorageKeys = "serverUrl" | "authToken";
+
+type StoredValues = {
+  serverUrl?: string;
+  authToken?: string;
+};
+
+export async function getKey(key: StorageKeys): Promise<string | null> {
+  const res = await browser.storage.local.get(key);
+  const value = (res as StoredValues)[key];
+  return typeof value === "string" ? value : null;
+}
+
+export async function setKey(key: StorageKeys, value: string): Promise<boolean> {
+  await browser.storage.local.set({ [key]: value });
+  return true;
+}
+
+export async function removeKey(key: StorageKeys): Promise<void> {
+  await browser.storage.local.remove(key);
+}
